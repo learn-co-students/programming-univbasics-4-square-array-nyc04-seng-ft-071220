9efab88dd4268bef63e62933a08f32bbcaa052ce
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-15 20:54:26 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array=[]
+  array.length.times do |index|
+    new_array.push(array[index]**2)
+  end
+  p new_array# your code here
 end
+
+square_array([1,2,3])
