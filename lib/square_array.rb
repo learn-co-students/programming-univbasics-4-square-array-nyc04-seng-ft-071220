708f6724fa36c1f37dbe402dcd3ed9381c30b4c3
Bella Evalle <bellaevalle@yahoo.com>
2020-07-16 19:25:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array (array)
+count = 0 
+while count < array.length do 
+ final_result = array[count] * array[count]
+ count += 1
+end
+ final_result
 end
