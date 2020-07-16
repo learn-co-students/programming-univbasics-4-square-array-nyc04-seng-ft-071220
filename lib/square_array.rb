b23e955f241a9b135bc5007c747adba624838b58
jade decordova <lacroixjaded@gmail.com>
2020-07-16 18:11:27 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared = []
+  counter = 0
+  until counter == array.length
+        square = array[counter] ** 2
+        squared << square
+        counter += 1
+  end
+  squared
 end
