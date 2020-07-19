be049e4be93590ce5e9160296628ef822a818fa2
hortencia c <hortenciaecisneros@gmail.com>
2020-07-19 00:32:45 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
+
 def square_array(array)
-  # your code here
+
+  counter = 0 
+  new_numbers =[]
+  while array.length > counter do 
+     num = array[counter]
+    new_numbers.push(num*num)
+      counter += 1 
+  end 
+  return new_numbers
+# num * num
 end
+  
